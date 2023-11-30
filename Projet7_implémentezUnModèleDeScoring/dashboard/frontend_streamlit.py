3f0b7c8496b1e5a3517f154dad3c7c960e72db2b
Barry Tidiane <tidibarry@yahoo.fr>
2023-11-30 09:07:17 +0100

    add explainer local

--- Projet7_implémentezUnModèleDeScoring/dashboard/frontend_streamlit.py
+++ Projet7_implémentezUnModèleDeScoring/dashboard/frontend_streamlit.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import pandas as pd
 from io import StringIO
+import joblib
+import shap
 
+# Fonction pour obtenir la liste des clients
 def get_client_list():
     response = requests.get('http://localhost:5000/get_client_list')
     
@@ -45,37 +48,46 @@ def get_original_data(url):
 url = 'http://localhost:5000/original_data'
 original_data= get_original_data(url)
 
-
-# def predict_credit_score(user_features):
-#     # Appeler l'endpoint pour obtenir les prédictions
-#     response = requests.post('http://localhost:5000/predict_credit_score', json={"features": user_features})
-
-#     if response.status_code == 200:
-#         prediction = response.json()["prediction"]
-#         st.write(f"Probabilité de faillite prédite : {prediction}")
-#     else:
-#         st.error("Erreur lors de la prédiction du score de crédit.")
-
+# Fonction pour effectuer la prédiction
 def get_prediction(data):
-    response = requests.post('http://localhost:5000/predict')
-    return response.json(data)
+    try:
+        response = requests.post('http://localhost:5000/predict', json=data)
+        response.raise_for_status()  # Lève une exception pour les codes d'erreur HTTP
+        response_data = response.json()
+        return response_data["prediction"][0]
+    except requests.exceptions.RequestException as e:
+        st.error(f"Erreur lors de la prédiction : {str(e)}")
+        return {"error": "Échec de récupération de la prédiction"}
 
-   
-# def apply_explainer_local(client_id):
-#     response = requests.post('http://localhost:5000/apply_explainer_local', json={'client_id': client_id})
+# Fonction pour obtenir les valeurs SHAP
+def get_shap_values(data):
+    response = requests.post('http://localhost:5000/explain', json=data)
+    if response.status_code == 200:
+        response_data = response.json()
+        shap_summary_plot_path = response_data.get("shap_summary_plot")
 
-#     if response.status_code == 200 and response.content:
-#         return response.json().get('explainer_result')
-#     else:
-#         st.error("Erreur lors de l'application de l'explainer local.")
-#         return None
-    
+        if shap_summary_plot_path:
+            st.image(shap_summary_plot_path, caption='SHAP Summary Plot', use_column_width=True)
+        else:
+            st.warning("Failed to generate SHAP Summary Plot.")
+
+    else:
+        st.warning("Failed to get SHAP values.")
+
+
+
+def columns_names():
+        cols_names = joblib.load('cols_names.joblib')
+        return cols_names
+
+
+
 def create_gauge(bid_price, ask_price, current_price, spread):
     fig = go.Figure()
 
     fig.add_trace(go.Indicator(
         mode="gauge+number+delta",
-        title={'text': "Score"},
+        title={'text': "Probabilité de faillite prédite"},
         delta={'reference': ask_price, 'relative': False, 'increasing': {'color': "RebeccaPurple"}, 'decreasing': {'color': "RoyalBlue"}},
         value=current_price,
         domain={'x': [0, 1], 'y': [0, 1]},
@@ -119,7 +131,7 @@ def main():
         st.write(f"Genre du client: {original_data[original_data['SK_ID_CURR'] == selected_client]['CODE_GENDER'].iloc[0]}")
         st.write(f"Âge : {int(original_data[original_data['SK_ID_CURR'] == selected_client]['DAYS_BIRTH'].iloc[0]/-365)}")
         st.write(f" Indicateur de possession d'une voiture: {original_data[original_data['SK_ID_CURR'] == selected_client]['FLAG_OWN_CAR'].iloc[0]}")
-        st.write(f"Indicateur de possession de biens immobiliers: {df[df['SK_ID_CURR'] == selected_client]['FLAG_OWN_REALTY'].iloc[0]}")
+        st.write(f"Indicateur de possession de biens immobiliers: {original_data[original_data['SK_ID_CURR'] == selected_client]['FLAG_OWN_REALTY'].iloc[0]}")
         st.write(f"Montant mensuel à rembourer: {original_data[original_data['SK_ID_CURR'] == selected_client]['AMT_ANNUITY'].iloc[0]}")
         st.write(f"Montant total du crédit demandé: {original_data[original_data['SK_ID_CURR'] == selected_client]['AMT_CREDIT'].iloc[0]}")
         st.write(f"Montant total du revenu du client: {original_data[original_data['SK_ID_CURR'] == selected_client]['AMT_INCOME_TOTAL'].iloc[0]}")
@@ -127,7 +139,7 @@ def main():
         #st.write(df)
 
 
-     # Ajouter la possibilité de saisir des features pour la prédiction
+    # Ajouter la possibilité de saisir des features pour la prédiction
     st.sidebar.subheader("Saisir des features pour la prédiction :")
     feature1 = st.sidebar.number_input("CODE_GENDER", value=df[df['SK_ID_CURR'] == selected_client]['CODE_GENDER'].iloc[0])
     feature2 = st.sidebar.number_input("DAYS_BIRTH", value=int(df[df['SK_ID_CURR'] == selected_client]['DAYS_BIRTH'].iloc[0] / -365))
@@ -136,50 +148,33 @@ def main():
 
     user_features = [feature1, feature2, feature3, feature4]
 
-    if st.sidebar.button("Prédire le score de crédit", key="predict_button", on_click=lambda: get_prediction(df[df['SK_ID_CURR'] == selected_client].to_json(orient = "split"))):
-        # Utilisez la méthode POST en cliquant sur le bouton de prédiction
+    if st.sidebar.button("Prédire le score de crédit", key="predict_button"):
+        features = columns_names()
+        selected_client_data = df[df['SK_ID_CURR'] == selected_client][features]
+        prediction = get_prediction(selected_client_data.to_dict(orient='records')[0])
+        st.session_state.selected_client_data = selected_client_data  # Stocker les données dans la variable de session
+        st.write(prediction)
 
-        pass
-
-
-
-    # # Visualisation du score et interprétation
-    # st.header("Score de Crédit et Interprétation")
-    # st.write(f"Score du client : {client_info.get('prediction', 'N/A')}")
-
-    # Afficher les informations spécifiques du client
-    client_info = df[df['SK_ID_CURR'] == selected_client].to_dict(orient='records')[0]
-
-    # Jauge de score
-    # Créer la jauge
-    bid_price = 100
-    ask_price = 120
-    current_price = float(client_info.get('prediction', 0.5))  # Remplacer par la valeur réelle du score
-    spread = 5
-
-    gauge_placeholder = st.empty()
-
-    # Créer la jauge et afficher dans le placeholder
-    fig = create_gauge(bid_price, ask_price, current_price, spread)
-    with gauge_placeholder:
+        fig = create_gauge(0, 1, prediction, 0.1)
         st.plotly_chart(fig, use_container_width=True)
 
+    if st.sidebar.button("Expliquer la prédiction", key="explain_button"):
+        if st.session_state.selected_client_data is not None:
+            shap_values = get_shap_values(st.session_state.selected_client_data.to_dict(orient='records')[0])
 
-   
-    # # Système de filtre
-    # st.sidebar.header("Filtres")
-    # # Ajoutez des options de filtre selon vos besoins
+            # Affichez les valeurs SHAP pour le débogage
+            st.write("SHAP Values:", shap_values)
 
-    # # Comparaison des informations descriptives
-    # st.header("Comparer les informations descriptives")
-    # # Ajoutez des éléments interactifs pour la comparaison
+            # Assurez-vous que les valeurs SHAP sont de la bonne forme
+            if shap_values is not None and len(shap_values.shape) == 2:
+                # Visualiser les valeurs SHAP sous forme de résumé
+                shap.summary_plot(shap_values, st.session_state.selected_client_data)
+            else:
+                st.warning("Les valeurs SHAP ne sont pas dans le format attendu.")
+        else:
+            st.warning("Veuillez d'abord effectuer une prédiction.")
 
-    # # Appliquer l'explainer local et afficher les résultats
-    # explainer_result = apply_explainer_local(selected_client)
 
-    # if explainer_result:
-    #     st.header("Explainer Local")
-    #     st.pyplot(explainer_result)  # Afficher le graphique de l'explainer local)
 
 if __name__ == "__main__":
     main()
