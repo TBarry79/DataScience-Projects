92b31a37bfd1b2a15d87e163229365ecb7457228
Barry Tidiane <tidibarry@yahoo.fr>
2023-12-15 16:37:43 +0100

    dashboard_update_v2

--- Projet7_implémentezUnModèleDeScoring/dashboard/frontend_streamlit.py
+++ Projet7_implémentezUnModèleDeScoring/dashboard/frontend_streamlit.py
@@ -7,6 +7,7 @@ from io import StringIO
 import joblib
 import shap
 import numpy as np  
+from streamlit_shap import st_shap
 import base64
 
 # Fonction pour obtenir la liste des clients
@@ -35,6 +36,7 @@ url = 'http://localhost:5000/full_dataframe'
 df = get_dataframe_from_api(url)
 
 
+
 # Appeler l'endpoint pour obtenir le DataFrame d'origine
 def get_original_data(url):
     try:
@@ -50,7 +52,8 @@ def get_original_data(url):
 url = 'http://localhost:5000/original_data'
 original_data= get_original_data(url)
 
-# Fonction pour effectuer la prédiction
+
+ # Fonction pour effectuer la prédiction
 def get_prediction(data):
     try:
         response = requests.post('http://localhost:5000/predict', json=data)
@@ -59,52 +62,38 @@ def get_prediction(data):
         return response_data["prediction"][0]
     except requests.exceptions.RequestException as e:
         st.error(f"Erreur lors de la prédiction : {str(e)}")
-        return {"error": "Échec de récupération de la prédiction"}
-
-# Fonction pour obtenir les valeurs SHAP
-def get_shap_values(data):
-    response = requests.post('http://localhost:5000/explain', json=data)
-    
-    if response.status_code == 200:
-        response_data = response.json()
-        shap_values_list = response_data.get("shap_values")
-        shap_summary_plot_data = response_data.get("shap_summary_plot_data")
-
-        if shap_summary_plot_data:
-            # Convertir la chaîne base64 en bytes
-            shap_summary_plot_data_bytes = base64.b64decode(shap_summary_plot_data)
-            
-            # Afficher l'image dans Streamlit
-            st.markdown(f'<img src="data:image/png;base64,{base64.b64encode(shap_summary_plot_data_bytes).decode()}" alt="SHAP Summary Plot">', unsafe_allow_html=True)
-            
-            # Retourner les valeurs SHAP pour une utilisation ultérieure si nécessaire
-            return shap_values_list
-        else:
-            st.warning("Échec de la génération du tracé récapitulatif SHAP.")
-            return None
-
-    else:
-        st.warning("Échec de l'obtention des valeurs SHAP.")
-        return None
-    
-
-# Fonction pour obtenir la contribution SHAP d'une seule fonctionnalité du backend
-def get_shap_contribution(data):
-    response = requests.post('http://localhost:5000/get_shap_contribution', json=data)
-    
-    if response.status_code == 200:
-        return True
-    else:
-        st.warning("Échec de l'obtention de la contribution SHAP.")
-        return False
+        return {"error": "Échec de récupération de la prédiction"}   
 
 
+# Fonction pour récupérer les noms des features
 def columns_names():
         cols_names = joblib.load('cols_names.joblib')
         return cols_names
 
 
+# Fonction pour récupérer le modele du backend
+def load_model_from_backend():
+    url = "http://localhost:5000/load_model"  # Remplacez par l'URL de votre serveur Flask
+    try:
+        response = requests.get(url)
+        response_data = response.json()
+        
+        if "model_path" in response_data:
+            # Charger le modèle depuis le fichier temporaire
+            return joblib.load(response_data["model_path"])
+        elif "error" in response_data:
+            st.error(f"Erreur lors du chargement du modèle: {response_data['error']}")
+        else:
+            st.error("Réponse inattendue du serveur.")
+    except Exception as e:
+        st.error(f"Erreur de récupération du modèle: {str(e)}")
+    return None
 
+# Charger le modèle depuis le backend
+model = load_model_from_backend()
+
+
+# Fonction pour la construction du jauge de score
 def create_gauge(bid_price, ask_price, current_price, spread):
     fig = go.Figure()
 
@@ -135,13 +124,14 @@ def create_gauge(bid_price, ask_price, current_price, spread):
 
     return fig
 
+
 def main():
 
     st.title("Dashboard de Scoring de Crédit - Prêt à Dépenser")
     st.subheader("Auteur : TIDIANE Barry")
 
     
-    selected_client = st.selectbox("Sélectionnez un client :", options=df['SK_ID_CURR'])
+    selected_client = st.selectbox("Sélectionnez un client :", options=df['ID'])
     st.write(selected_client)
     
 
@@ -158,27 +148,30 @@ def main():
         st.write(f"Montant mensuel à rembourer: {original_data[original_data['SK_ID_CURR'] == selected_client]['AMT_ANNUITY'].iloc[0]}")
         st.write(f"Montant total du crédit demandé: {original_data[original_data['SK_ID_CURR'] == selected_client]['AMT_CREDIT'].iloc[0]}")
         st.write(f"Montant total du revenu du client: {original_data[original_data['SK_ID_CURR'] == selected_client]['AMT_INCOME_TOTAL'].iloc[0]}")
-        # st.write(f"Revenu provenant du travail: {df[df['SK_ID_CURR'] == selected_client]['NAME_INCOME_TYPE_Working'].iloc[0]}")
         #st.write(df)
 
-    # Saisie des features pour la prédiction
+     # Saisie des features pour la prédiction
     st.sidebar.subheader("Saisir des features pour la prédiction :")
-    feature1 = st.sidebar.number_input("CODE_GENDER", value=df[df['SK_ID_CURR'] == selected_client]['CODE_GENDER'].iloc[0])
-    feature2 = st.sidebar.number_input("DAYS_BIRTH", value=int(df[df['SK_ID_CURR'] == selected_client]['DAYS_BIRTH'].iloc[0] / -365))
-    feature3 = st.sidebar.number_input("AMT_ANNUITY", value=df[df['SK_ID_CURR'] == selected_client]['AMT_ANNUITY'].iloc[0])
-    feature4 = st.sidebar.number_input("AMT_CREDIT", value=df[df['SK_ID_CURR'] == selected_client]['AMT_CREDIT'].iloc[0])
+    feature1 = st.sidebar.number_input("CODE_GENDER", value=df[df['ID'] == selected_client]['CODE_GENDER'].iloc[0])
+    feature2 = st.sidebar.number_input("DAYS_BIRTH", value=int(df[df['ID'] == selected_client]['DAYS_BIRTH'].iloc[0] / -365))
+    feature3 = st.sidebar.number_input("AMT_ANNUITY", value=df[df['ID'] == selected_client]['AMT_ANNUITY'].iloc[0])
+    feature4 = st.sidebar.number_input("AMT_CREDIT", value=df[df['ID'] == selected_client]['AMT_CREDIT'].iloc[0])
 
     user_features = [feature1, feature2, feature3, feature4]
 
     # Prédiction du score de crédit
     if st.sidebar.button("Prédire le score de crédit", key="predict_button"):
-        #features = df.columns[3:588].values
+        #features = df.columns[3:574].values
         # Remplacer par les valeurs d'origines des features
         features = columns_names()
-        selected_client_data = df[df['SK_ID_CURR'] == selected_client][features]
+        selected_client_data = df[df['ID'] == selected_client][features]
         prediction = get_prediction(selected_client_data.to_dict(orient='records')[0])
         # Arrondir la prédiction à deux chiffres après la virgule et convertir en pourcentage
-        prediction = round(prediction, 2) * 100
+        if prediction is not None:
+            prediction = round(prediction, 2) * 100
+            st.write("Prediction de probabilité de défaut de crédit :", prediction, "%")
+        else:
+            st.warning("Aucune prédiction disponible.")
         prediction_with_percent = f"{prediction}%"
         st.session_state.selected_client_data = selected_client_data  # Stocker les données dans la variable de session
         st.write(prediction_with_percent)
@@ -187,54 +180,79 @@ def main():
         st.plotly_chart(fig, use_container_width=True)
 
 
-    # Calcul de la contribution SHAP pour une seule feature
     if st.sidebar.button("Expliquer la prédiction", key="explain_button"):
-        if st.session_state.selected_client_data is not None:
-            shap_values = get_shap_values(st.session_state.selected_client_data.to_dict(orient='records')[0])
+        # Charger le modèle depuis le backend
+        model = load_model_from_backend()
 
-            # Affichez les valeurs SHAP pour le débogage
-            st.write("SHAP Values:", shap_values)
+        # Supprimer la colonne 'ID' de df
+        df_without_id = df.drop('ID', axis=1)
 
-            if shap_values is not None and len(shap_values) == 1:
-                # Accéder à la première sous-liste des valeurs SHAP
-                shap_values_list = shap_values[0]
+        # Créer un explainer SHAP avec le modèle et le DataFrame sans la colonne 'ID'
+        explainer = shap.Explainer(model, df_without_id)
 
-                # Assurez-vous que la sous-liste a la longueur attendue
-                if len(shap_values_list) == 2:
-                    # Visualiser les valeurs SHAP sous forme de résumé
-                    shap.summary_plot(shap_values_list, st.session_state.selected_client_data)
-                else:
-                    st.warning("Les valeurs SHAP ne sont pas dans le format attendu.")
-            else:
-                st.warning("Échec de l'obtention des valeurs SHAP..")
+        # Calculer les valeurs SHAP
+        shap_values = explainer.shap_values(df_without_id)
+        
+        # Créer un objet Explanation
+        explanation = shap.Explanation(values=shap_values, base_values=explainer.expected_value, data=df_without_id)
+        
+        st_shap(shap.plots.waterfall(explanation[0]), height=300)
+        st_shap(shap.plots.beeswarm(explanation), height=300)
 
-    
-    # Sélectionner une observation spécifique à interpréter (par exemple, la première observation)
-    selected_observation  = {
+
+        # Sélectionner la variable pour laquelle vous souhaitez créer le graphique de dépendance
+        feature_name = "EXT_SOURCE_3"  # Remplacez par le nom de la variable d'intérêt
+
+        # Créer le graphique de dépendance SHAP
+        st_shap(shap.dependence_plot(feature_name, shap_values, df_without_id, show=False))
+
+
+
+    user_features = {}
+    selected_observation = {
         "CODE_GENDER": "CODE_GENDER",
-        "DAYS_BIRTH": "DAYS_BIRTH",  
-        "AMT_ANNUITY": "AMT_ANNUITY",  
-        "AMT_CREDIT": "AMT_CREDIT",  
-        "EXT_SOURCE_2": "EXT_SOURCE_2"  
+        "DAYS_BIRTH": "DAYS_BIRTH",
+        "AMT_ANNUITY": "AMT_ANNUITY",
+        "AMT_CREDIT": "AMT_CREDIT",
+        "EXT_SOURCE_3": "EXT_SOURCE_3"
     }
 
-    # Sélectionner une observation spécifique à interpréter
-    selected_observation = st.sidebar.selectbox("Sélectionnez une feature pour la contribution SHAP", options=list(selected_observation.keys()))
+    
+    # Assuming 'selected_feature' is the selected feature
+    selected_feature = st.sidebar.selectbox("Sélectionnez une feature pour la contribution SHAP", options=list(selected_observation.keys()))
 
-    # Calcul de la contribution SHAP pour la feature sélectionnée
+    # Define default values based on descriptive statistics (replace with actual values)
+    default_feature_values = {
+        "CODE_GENDER": df[selected_feature].mode().iloc[0],
+        "DAYS_BIRTH": df[selected_feature].median(),
+        "AMT_ANNUITY": df[selected_feature].mean(),
+        "AMT_CREDIT": df[selected_feature].mean(),
+        "EXT_SOURCE_3": df[selected_feature].median()
+    }
+
+    # Get the actual feature value from the user
+    actual_feature_value = st.sidebar.number_input(f"Valeur pour {selected_feature}", value=default_feature_values.get(selected_feature, 0.0))
+
+    # Button to trigger SHAP plot
     if st.sidebar.button("Afficher la contribution SHAP", key="shap_button"):
-        # Envoyer la feature sélectionnée au backend
-        feature_name = selected_observation
-        data = {"feature_name": feature_name}
-        success = get_shap_contribution(data)
-        
-        if success:
-            st.success(f"La contribution SHAP pour {feature_name} a été calculée avec succès et sauvegardée.")
-           
-            shap.dependence_plot(feature_name, shap_values, pd.DataFrame([selected_observation]), show=True)
-        else:
-            st.error("Erreur lors du calcul de la contribution SHAP.")
+        # Update user_features with the actual feature value
+        user_features[selected_feature] = actual_feature_value
 
+        # Supprimer la colonne 'ID' de df
+        df_without_id = df.drop('ID', axis=1)
+    
+        # Create a DataFrame with the selected features and the updated user value
+        user_df = pd.DataFrame([user_features], columns=df_without_id.columns)
+
+        # Charger le modèle depuis le backend
+        model = load_model_from_backend()
+
+        # Calculate SHAP values for the updated observation
+        explainer = shap.Explainer(model, df_without_id)
+        shap_values = explainer.shap_values(user_df)
+
+        # Display the SHAP dependence plot
+        st_shap(shap.dependence_plot(str(selected_feature), shap_values, user_df, show=True))
 
 if __name__ == "__main__":
     main()
--- Projet7_implémentezUnModèleDeScoring/dashboard/start.sh
+++ Projet7_implémentezUnModèleDeScoring/dashboard/start.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 python backend_flask.py & streamlit run frontend_streamlit.py
+
+# # Lancement du backend Flask avec Gunicorn
+# gunicorn -w 4 -b 0.0.0.0:5000 backend_flask:app &
+
+# # Attendez que le backend soit prêt (ajuster le délai si nécessaire)
+# sleep 5
+
+# # Lancement du frontend Streamlit
+# streamlit run frontend_streamlit.py
